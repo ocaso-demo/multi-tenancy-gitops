fe4ecea0cee833f0f6cfad8f10b5b6d46dde2e70
Jesus Almaraz <jesus.mah@gmail.com>
2022-10-13 00:45:45 +0200

    adding new ROKS cluster ArgoCD app to kustomization

--- 0-bootstrap/spokeclusters/spoke-cluster-template/new-cluster.sh
+++ 0-bootstrap/spokeclusters/spoke-cluster-template/new-cluster.sh
@@ -24,7 +24,11 @@ find . -name '*.yaml' -print0 |
 rm new-cluster.sh
 
 nl=$'\n'
+# Update RHACM ArgoCD Project to allow argo to interact with the namespace for the newly created ROKS cluster
 sed -i'.bak' -e 's@  destinations:@  destinations:'"\\${nl}"'  - namespace: '"${NEW_CLUSTER}\\${nl}"'    server: https://kubernetes.default.svc@g' ../../hub/4-rhacm/4-rhacm.yaml
 rm "../../hub/4-rhacm/4-rhacm.yaml.bak"
+# Update RHACM layer kustomization file to add the new ArgoCD Application that will take care of importing the newly created ROKS cluster
+sed -i'.bak' -e 's@# RHACM Imported Clusters@# RHACM Imported Clusters'"\\${nl}"'- argocd/clusters/imported/'"${NEW_CLUSTER}.yaml"'@g' ../../hub/4-rhacm/kustomization.yaml
+rm "../../hub/4-rhacm/kustomization.yaml.bak"
 
 echo "git commit and push changes now"
